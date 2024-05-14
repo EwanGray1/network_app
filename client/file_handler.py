23455280187964ffdaab954e2bbb331cbad327bf
EwanGray1 <161265972+EwanGray1@users.noreply.github.com>
2024-05-14 04:26:32 +0100

    Update file_handler.py

--- client/file_handler.py
+++ client/file_handler.py
@@ -1,8 +1,25 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/EwanGray1/network_app/blob/main/Client/file_handler.py</string>
-</dict>
-</plist>
+import json
+import xml.etree.ElementTree as ET
+import pickle
+
+class FileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        with open(self.file_path, 'r') as file:
+            return file.read()
+
+    def serialize(self, data_format):
+        data = self.read_file()
+        if data_format == 'json':
+            return json.dumps(data).encode()
+        elif data_format == 'xml':
+            root = ET.Element("root")
+            child = ET.SubElement(root, "data")
+            child.text = data
+            return ET.tostring(root)
+        elif data_format == 'binary':
+            return pickle.dumps(data)
+        else:
+            raise ValueError("Unsupported data format")
