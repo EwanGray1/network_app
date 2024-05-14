18ec2700b077b415721f0d5bf8f812bed6e2eeea
EwanGray1 <161265972+EwanGray1@users.noreply.github.com>
2024-05-14 04:17:59 +0100

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -3,8 +3,8 @@ import json
 import xml.etree.ElementTree as ET
 import pickle
 import os
-from Server.logger import setup_logging
-from Server.config import Config
+from server.logger import setup_logging
+from server.config import Config
 from cryptography.fernet import Fernet
 
 # Setup logging
