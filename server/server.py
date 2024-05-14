0f33ef869c588716e30cd151fbfa695280ca2924
EwanGray1 <161265972+EwanGray1@users.noreply.github.com>
2024-05-14 04:24:43 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,8 +1,54 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/EwanGray1/network_app/blob/main/Server/server.py</string>
-</dict>
-</plist>
+import socket
+import json
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from server.logger import setup_logging
+from server.config import Config
+from cryptography.fernet import Fernet
+
+# Setup logging
+logger = setup_logging()
+
+def deserialize_data(data, data_format):
+    if data_format == 'json':
+        return json.loads(data)
+    elif data_format == 'xml':
+        root = ET.fromstring(data)
+        return {child.tag: child.text for child in root}
+    elif data_format == 'binary':
+        return pickle.loads(data)
+    else:
+        raise ValueError("Unsupported data format")
+
+def decrypt_data(encrypted_data, key):
+    cipher_suite = Fernet(key)
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
+    return decrypted_data
+
+def main():
+    config = Config()
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((config.host, config.port))
+    server_socket.listen(5)
+    logger.info(f'Server started on {config.host}:{config.port}')
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        logger.info(f'Connection from {addr}')
+        data_format = client_socket.recv(1024).decode()
+        encrypted = client_socket.recv(1024).decode() == 'True'
+        key = client_socket.recv(1024) if encrypted else None
+        data = client_socket.recv(1024)
+        
+        if encrypted:
+            data = decrypt_data(data, key)
+        
+        data = deserialize_data(data, data_format)
+        logger.info(f'Received data: {data}')
+        
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
